cofran31/nuxeo

commit 77634a1aa8bd4d348a5505fd376f1cdb836c1c82

NXP-28839: extract plugin extension point for JSF plug (and other potential use cases), extract constants

modules/platform/nuxeo-apidoc-server/nuxeo-apidoc-webengine/src/main/java/org/nuxeo/apidoc/browse/Distribution.java
@@ -57,6 +57,7 @@ import org.apache.commons.logging.LogFactory;
 import org.nuxeo.apidoc.documentation.DocumentationService;
 import org.nuxeo.apidoc.export.ArchiveFile;
 import org.nuxeo.apidoc.listener.AttributesExtractorStater;
+import org.nuxeo.apidoc.plugin.Plugin;
 import org.nuxeo.apidoc.snapshot.DistributionSnapshot;
 import org.nuxeo.apidoc.snapshot.DistributionSnapshotDesc;
 import org.nuxeo.apidoc.snapshot.SnapshotFilter;
@@ -110,41 +111,44 @@ public class Distribution extends ModuleRoot {
     }
 
     public String getNavigationPoint() {
-        String currentUrl = getContext().getURL();
-        String navPoint = null;
-
-        if (currentUrl.contains("/listBundles")) {
-            navPoint = "listBundles";
-        } else if (currentUrl.contains("/listComponents")) {
-            navPoint = "listComponents";
-        } else if (currentUrl.contains("/listServices")) {
-            navPoint = "listServices";
-        } else if (currentUrl.contains("/listExtensionPoints")) {
-            navPoint = "listExtensionPoints";
-        } else if (currentUrl.contains("/listContributions")) {
-            navPoint = "listContributions";
-        } else if (currentUrl.contains("/listBundleGroups")) {
-            navPoint = "listBundleGroups";
-        } else if (currentUrl.contains("/viewBundleGroup")) {
-            navPoint = "viewBundleGroup";
-        } else if (currentUrl.contains("/viewComponent")) {
-            navPoint = "viewComponent";
-        } else if (currentUrl.contains("/viewService")) {
-            navPoint = "viewService";
-        } else if (currentUrl.contains("/viewExtensionPoint")) {
-            navPoint = "viewExtensionPoint";
-        } else if (currentUrl.contains("/viewContribution")) {
-            navPoint = "viewContribution";
-        } else if (currentUrl.contains("/viewBundle")) {
-            navPoint = "viewBundle";
-        } else if (currentUrl.contains("/listOperations")) {
-            navPoint = "listOperations";
-        } else if (currentUrl.contains("/viewOperation")) {
-            navPoint = "viewOperation";
-        } else if (currentUrl.contains("/doc")) {
-            navPoint = "documentation";
+        String url = getContext().getURL();
+        String point = null;
+        if (ApiBrowserConstants.check(url, ApiBrowserConstants.LIST_BUNDLEGROUPS)
+                || ApiBrowserConstants.check(url, ApiBrowserConstants.VIEW_BUNDLEGROUP)) {
+            point = ApiBrowserConstants.LIST_BUNDLEGROUPS;
+        } else if (ApiBrowserConstants.check(url, ApiBrowserConstants.LIST_BUNDLES)
+                || ApiBrowserConstants.check(url, ApiBrowserConstants.VIEW_BUNDLE)) {
+            point = ApiBrowserConstants.LIST_BUNDLES;
+        } else if (ApiBrowserConstants.check(url, ApiBrowserConstants.LIST_COMPONENTS)
+                || ApiBrowserConstants.check(url, ApiBrowserConstants.VIEW_COMPONENT)) {
+            point = ApiBrowserConstants.LIST_COMPONENTS;
+        } else if (ApiBrowserConstants.check(url, ApiBrowserConstants.LIST_SERVICES)
+                || ApiBrowserConstants.check(url, ApiBrowserConstants.VIEW_SERVICE)) {
+            point = ApiBrowserConstants.LIST_SERVICES;
+        } else if (ApiBrowserConstants.check(url, ApiBrowserConstants.LIST_EXTENSIONPOINTS)
+                || ApiBrowserConstants.check(url, ApiBrowserConstants.VIEW_EXTENSIONPOINT)) {
+            point = ApiBrowserConstants.LIST_EXTENSIONPOINTS;
+        } else if (ApiBrowserConstants.check(url, ApiBrowserConstants.LIST_CONTRIBUTIONS)
+                || ApiBrowserConstants.check(url, ApiBrowserConstants.VIEW_CONTRIBUTION)) {
+            point = ApiBrowserConstants.LIST_CONTRIBUTIONS;
+        } else if (ApiBrowserConstants.check(url, ApiBrowserConstants.LIST_OPERATIONS)
+                || ApiBrowserConstants.check(url, ApiBrowserConstants.VIEW_OPERATION)) {
+            point = ApiBrowserConstants.LIST_OPERATIONS;
+        } else if (ApiBrowserConstants.check(url, ApiBrowserConstants.VIEW_DOCUMENTATION)) {
+            point = ApiBrowserConstants.VIEW_DOCUMENTATION;
         }
-        return navPoint;
+        if (point == null) {
+            // check plugins
+            List<Plugin<?>> plugins = getSnapshotManager().getPlugins();
+            for (Plugin<?> plugin : plugins) {
+                point = plugin.getView(url);
+                if (point != null) {
+                    break;
+                }
+            }
+        }
+
+        return point;
     }
 
     @GET
@@ -552,10 +556,21 @@ public class Distribution extends ModuleRoot {
     }
 
     public static boolean showCurrentDistribution() {
-        return !(Framework.isBooleanPropertyTrue("org.nuxeo.apidoc.hide.current.distribution") || isSiteMode());
+        return !(Framework.isBooleanPropertyTrue(ApiBrowserConstants.PROPERTY_SITE_MODE) || isSiteMode());
     }
 
     public static boolean isSiteMode() {
-        return Framework.isBooleanPropertyTrue("org.nuxeo.apidoc.site.mode");
+        return Framework.isBooleanPropertyTrue(ApiBrowserConstants.PROPERTY_SITE_MODE);
     }
+
+    /**
+     * Generates the list of plugins that should be displayed in the menu.
+     */
+    public List<Plugin<?>> getPluginMenu() {
+        return getSnapshotManager().getPlugins()
+                                   .stream()
+                                   .filter(plugin -> !plugin.isHidden())
+                                   .collect(Collectors.toList());
+    }
+
 }

modules/platform/nuxeo-apidoc-server/nuxeo-apidoc-core/src/main/java/org/nuxeo/apidoc/adapters/BundleGroupDocAdapter.java
@@ -23,6 +23,7 @@ import java.util.List;
 
 import org.nuxeo.apidoc.api.BundleGroup;
 import org.nuxeo.apidoc.api.BundleInfo;
+import org.nuxeo.apidoc.api.NuxeoArtifact;
 import org.nuxeo.apidoc.api.QueryHelper;
 import org.nuxeo.apidoc.snapshot.DistributionSnapshot;
 import org.nuxeo.common.utils.Path;
@@ -45,7 +46,7 @@ public class BundleGroupDocAdapter extends BaseNuxeoArtifactDocAdapter implement
             doc = session.getDocument(new PathRef(targetPath));
         }
         doc.setPathInfo(containerPath, name);
-        doc.setPropertyValue("dc:title", bundleGroup.getName());
+        doc.setPropertyValue(NuxeoArtifact.TITLE_PROPERTY_PATH, bundleGroup.getName());
         doc.setPropertyValue(PROP_GROUP_NAME, bundleGroup.getName());
         doc.setPropertyValue(PROP_KEY, bundleGroup.getId());
         if (exist) {

modules/platform/nuxeo-apidoc-server/nuxeo-apidoc-core/src/main/java/org/nuxeo/apidoc/adapters/BundleInfoDocAdapter.java
@@ -27,6 +27,7 @@ import java.util.Map;
 
 import org.nuxeo.apidoc.api.BundleInfo;
 import org.nuxeo.apidoc.api.ComponentInfo;
+import org.nuxeo.apidoc.api.NuxeoArtifact;
 import org.nuxeo.apidoc.documentation.ResourceDocumentationItem;
 import org.nuxeo.common.utils.Path;
 import org.nuxeo.ecm.core.api.Blob;
@@ -48,7 +49,7 @@ public class BundleInfoDocAdapter extends BaseNuxeoArtifactDocAdapter implements
             doc = session.getDocument(new PathRef(targetPath));
         }
         doc.setPathInfo(containerPath, name);
-        doc.setPropertyValue("dc:title", bundleInfo.getBundleId());
+        doc.setPropertyValue(NuxeoArtifact.TITLE_PROPERTY_PATH, bundleInfo.getBundleId());
         doc.setPropertyValue(PROP_ARTIFACT_GROUP_ID, bundleInfo.getGroupId());
         doc.setPropertyValue(PROP_ARTIFACT_ID, bundleInfo.getArtifactId());
         doc.setPropertyValue(PROP_ARTIFACT_VERSION, bundleInfo.getArtifactVersion());
@@ -58,7 +59,7 @@ public class BundleInfoDocAdapter extends BaseNuxeoArtifactDocAdapter implements
         if (manifest != null) {
             Blob manifestBlob = Blobs.createBlob(manifest);
             manifestBlob.setFilename("MANIFEST.MF");
-            doc.setPropertyValue("file:content", (Serializable) manifestBlob);
+            doc.setPropertyValue(NuxeoArtifact.CONTENT_PROPERTY_PATH, (Serializable) manifestBlob);
         }
 
         if (exist) {
@@ -115,7 +116,7 @@ public class BundleInfoDocAdapter extends BaseNuxeoArtifactDocAdapter implements
     @Override
     public String getManifest() {
         try {
-            Blob mf = safeGet(Blob.class, "file:content", null);
+            Blob mf = safeGet(Blob.class, NuxeoArtifact.CONTENT_PROPERTY_PATH, null);
             if (mf == null) {
                 return "No MANIFEST.MF";
             }

modules/platform/nuxeo-apidoc-server/nuxeo-apidoc-core/src/main/java/org/nuxeo/apidoc/adapters/ComponentInfoDocAdapter.java
@@ -29,6 +29,7 @@ import org.nuxeo.apidoc.api.BundleInfo;
 import org.nuxeo.apidoc.api.ComponentInfo;
 import org.nuxeo.apidoc.api.ExtensionInfo;
 import org.nuxeo.apidoc.api.ExtensionPointInfo;
+import org.nuxeo.apidoc.api.NuxeoArtifact;
 import org.nuxeo.apidoc.api.QueryHelper;
 import org.nuxeo.apidoc.api.ServiceInfo;
 import org.nuxeo.apidoc.documentation.DocumentationHelper;
@@ -60,7 +61,7 @@ public class ComponentInfoDocAdapter extends BaseNuxeoArtifactDocAdapter impleme
             doc = session.getDocument(new PathRef(targetPath));
         }
         doc.setPathInfo(containerPath, name);
-        doc.setPropertyValue("dc:title", componentInfo.getName());
+        doc.setPropertyValue(NuxeoArtifact.TITLE_PROPERTY_PATH, componentInfo.getName());
         doc.setPropertyValue(PROP_COMPONENT_ID, componentInfo.getId());
         doc.setPropertyValue(PROP_COMPONENT_NAME, componentInfo.getName());
         doc.setPropertyValue(PROP_COMPONENT_CLASS, componentInfo.getComponentClass());
@@ -70,7 +71,7 @@ public class ComponentInfoDocAdapter extends BaseNuxeoArtifactDocAdapter impleme
 
         Blob xmlBlob = Blobs.createBlob(componentInfo.getXmlFileContent(), "text/xml", null,
                 componentInfo.getXmlFileName());
-        doc.setPropertyValue("file:content", (Serializable) xmlBlob);
+        doc.setPropertyValue(NuxeoArtifact.CONTENT_PROPERTY_PATH, (Serializable) xmlBlob);
 
         if (exist) {
             doc = session.saveDocument(doc);
@@ -153,7 +154,7 @@ public class ComponentInfoDocAdapter extends BaseNuxeoArtifactDocAdapter impleme
     @Override
     public String getXmlFileContent() throws IOException {
         try {
-            Blob xml = safeGet(Blob.class, "file:content", null);
+            Blob xml = safeGet(Blob.class, NuxeoArtifact.CONTENT_PROPERTY_PATH, null);
             if (xml.getEncoding() == null || "".equals(xml.getEncoding())) {
                 xml.setEncoding("utf-8");
             }
@@ -166,7 +167,7 @@ public class ComponentInfoDocAdapter extends BaseNuxeoArtifactDocAdapter impleme
 
     @Override
     public String getXmlFileName() {
-        Blob xml = safeGet(Blob.class, "file:content", null);
+        Blob xml = safeGet(Blob.class, NuxeoArtifact.CONTENT_PROPERTY_PATH, null);
         return xml == null ? "" : xml.getFilename() == null ? "" : xml.getFilename();
     }
 

modules/platform/nuxeo-apidoc-server/nuxeo-apidoc-core/src/main/java/org/nuxeo/apidoc/adapters/ExtensionInfoDocAdapter.java
@@ -27,6 +27,7 @@ import org.dom4j.DocumentException;
 import org.nuxeo.apidoc.api.BundleInfo;
 import org.nuxeo.apidoc.api.ComponentInfo;
 import org.nuxeo.apidoc.api.ExtensionInfo;
+import org.nuxeo.apidoc.api.NuxeoArtifact;
 import org.nuxeo.apidoc.api.VirtualNodesConsts;
 import org.nuxeo.apidoc.documentation.ContributionItem;
 import org.nuxeo.apidoc.documentation.DocumentationHelper;
@@ -59,7 +60,7 @@ public class ExtensionInfoDocAdapter extends BaseNuxeoArtifactDocAdapter impleme
             doc = session.getDocument(new PathRef(targetPath));
         }
         doc.setPathInfo(containerPath, name);
-        doc.setPropertyValue("dc:title", id);
+        doc.setPropertyValue(NuxeoArtifact.TITLE_PROPERTY_PATH, id);
 
         doc.setPropertyValue(PROP_CONTRIB_ID, id);
         doc.setPropertyValue(PROP_DOC, xi.getDocumentation());
@@ -67,7 +68,7 @@ public class ExtensionInfoDocAdapter extends BaseNuxeoArtifactDocAdapter impleme
         doc.setPropertyValue(PROP_TARGET_COMPONENT_NAME, xi.getTargetComponentName().getName());
 
         Blob xmlBlob = Blobs.createBlob(xi.getXml(), "text/xml", null, "contrib.xml"); // !!!!!
-        doc.setPropertyValue("file:content", (Serializable) xmlBlob);
+        doc.setPropertyValue(NuxeoArtifact.CONTENT_PROPERTY_PATH, (Serializable) xmlBlob);
 
         if (exist) {
             doc = session.saveDocument(doc);
@@ -110,7 +111,7 @@ public class ExtensionInfoDocAdapter extends BaseNuxeoArtifactDocAdapter impleme
     @Override
     public String getXml() {
         try {
-            Blob xml = safeGet(Blob.class, "file:content", null);
+            Blob xml = safeGet(Blob.class, NuxeoArtifact.CONTENT_PROPERTY_PATH, null);
             if (xml == null) {
                 return "";
             }

modules/platform/nuxeo-apidoc-server/nuxeo-apidoc-core/src/main/java/org/nuxeo/apidoc/adapters/ExtensionPointInfoDocAdapter.java
@@ -26,6 +26,7 @@ import org.nuxeo.apidoc.api.BundleInfo;
 import org.nuxeo.apidoc.api.ComponentInfo;
 import org.nuxeo.apidoc.api.ExtensionInfo;
 import org.nuxeo.apidoc.api.ExtensionPointInfo;
+import org.nuxeo.apidoc.api.NuxeoArtifact;
 import org.nuxeo.apidoc.api.QueryHelper;
 import org.nuxeo.apidoc.api.VirtualNodesConsts;
 import org.nuxeo.apidoc.documentation.DocumentationHelper;
@@ -52,7 +53,7 @@ public class ExtensionPointInfoDocAdapter extends BaseNuxeoArtifactDocAdapter im
             doc = session.getDocument(new PathRef(targetPath));
         }
         doc.setPathInfo(containerPath, name);
-        doc.setPropertyValue("dc:title", xpi.getId());
+        doc.setPropertyValue(NuxeoArtifact.TITLE_PROPERTY_PATH, xpi.getId());
 
         doc.setPropertyValue(PROP_NAME, xpi.getName());
         doc.setPropertyValue(PROP_EP_ID, xpi.getId());

modules/platform/nuxeo-apidoc-server/nuxeo-apidoc-core/src/main/java/org/nuxeo/apidoc/adapters/OperationInfoDocAdapter.java
@@ -24,6 +24,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.nuxeo.apidoc.api.NuxeoArtifact;
 import org.nuxeo.apidoc.api.OperationInfo;
 import org.nuxeo.common.utils.Path;
 import org.nuxeo.ecm.automation.OperationDocumentation.Param;
@@ -157,7 +158,7 @@ public class OperationInfoDocAdapter extends BaseNuxeoArtifactDocAdapter impleme
             doc = session.createDocumentModel(TYPE_NAME);
             doc.setPathInfo(containerPath, name);
         }
-        doc.setPropertyValue("dc:title", oi.getName());
+        doc.setPropertyValue(NuxeoArtifact.TITLE_PROPERTY_PATH, oi.getName());
         doc.setPropertyValue(PROP_NAME, oi.getName());
         doc.setPropertyValue(PROP_ALIASES, oi.getAliases());
         doc.setPropertyValue(PROP_VERSION, oi.getVersion());

modules/platform/nuxeo-apidoc-server/nuxeo-apidoc-core/src/main/java/org/nuxeo/apidoc/adapters/ServiceInfoDocAdapter.java
@@ -19,6 +19,7 @@
 package org.nuxeo.apidoc.adapters;
 
 import org.nuxeo.apidoc.api.BundleInfo;
+import org.nuxeo.apidoc.api.NuxeoArtifact;
 import org.nuxeo.apidoc.api.ServiceInfo;
 import org.nuxeo.apidoc.api.VirtualNodesConsts;
 import org.nuxeo.common.utils.Path;
@@ -44,7 +45,7 @@ public class ServiceInfoDocAdapter extends BaseNuxeoArtifactDocAdapter implement
             doc = session.getDocument(new PathRef(targetPath));
         }
         doc.setPathInfo(containerPath, name);
-        doc.setPropertyValue("dc:title", si.getId());
+        doc.setPropertyValue(NuxeoArtifact.TITLE_PROPERTY_PATH, si.getId());
 
         doc.setPropertyValue(PROP_CLASS_NAME, si.getId());
         doc.setPropertyValue(PROP_COMPONENT_ID, si.getComponentId());

commit d5ef823dd87aff327ae1d8346f5df6643bad90a3

NXP-29050: fix explorer property retrieval (regression introduced by 77634a1aa8bd4d348a5505fd376f1cdb836c1c82 with NXP-28839)

modules/platform/nuxeo-apidoc-server/nuxeo-apidoc-webengine/src/main/java/org/nuxeo/apidoc/browse/Distribution.java
@@ -573,7 +573,7 @@ public class Distribution extends ModuleRoot {
     }
 
     public static boolean showCurrentDistribution() {
-        return !(Framework.isBooleanPropertyTrue(ApiBrowserConstants.PROPERTY_SITE_MODE) || isSiteMode());
+        return !(Framework.isBooleanPropertyTrue(ApiBrowserConstants.PROPERTY_HIDE_CURRENT_DISTRIBUTION) || isSiteMode());
     }
 
     public static boolean isSiteMode() {